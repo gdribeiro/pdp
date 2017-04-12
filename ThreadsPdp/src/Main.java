134771295a95ef49f2c8ecc8ff373eae6e4ad286
Giovane Ribeiro <gdribeiro@users.noreply.github.com>
2017-04-12 14:12:06 -0300

    Update Main.java

--- ThreadsPdp/src/Main.java
+++ ThreadsPdp/src/Main.java
@@ -1,12 +1,16 @@
-
-
-
 public class Main {
 
 	public static void main(String[] args) {
 		for (Integer i = 0; i < 10; i++) {
 			MinhaThreadThread t = new MinhaThreadThread(i.toString());
 			t.start();
+		}
+	}
+	
+	public static void main(String[] args) {
+		for (Integer i = 0; i < 10; i++) {
+			MinhaThreadRunnable t = new MinhaThreadRunnable(i.toString());
+			(new Thread(MinhaThreadRunnable)).start();
 		}
 	}
 }
