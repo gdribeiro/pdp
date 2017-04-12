b585d66f99e698b9fe007a84070ccea3a10b9bae
Giovane Ribeiro <gdribeiro@users.noreply.github.com>
2017-04-12 14:09:00 -0300

    Update MinhaThreadRunnable.java

--- ThreadsPdp/src/MinhaThreadRunnable.java
+++ ThreadsPdp/src/MinhaThreadRunnable.java
@@ -1,13 +1,15 @@
 
 public class MinhaThreadRunnable implements Runnable {
+	String nome;
+	
+	public MinhaThreadThread(String nome) {
+		this.nome = nome;
+	}
 
 	@Override
 	public void run() {
-		
-		
+		System.out.println(nome);
+		System.out.println(Thread.currentThread().getPriority());
+		System.out.println(Thread.currentThread());
 	}
-
-	
-	
-	
 }
